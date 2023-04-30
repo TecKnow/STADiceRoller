9acaac8715e89cf4b9a1f2c3fa6bdccbd0c15a60
David P. Perkins <tecknow@grumbleware.com>
2023-04-29 22:27:07 -0500

    Change InputArea fields to MUI components

--- sta-dice-roller/src/components/InputArea.js
+++ sta-dice-roller/src/components/InputArea.js
@@ -1,6 +1,8 @@
 import { Fragment } from "react";
-import Switch from '@mui/material/Switch'
-import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from "@mui/material/Switch";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Slider from "@mui/material/Slider";
+import Box from "@mui/material/Box";
 import FlexibleNumericInput from "./DeferredNumericInput";
 
 export default function InputArea({
@@ -23,54 +25,70 @@ export default function InputArea({
   complicationsRange,
   complicationsRangeMin,
   complicationsRangeMax,
-  setComplicationsRange
+  setComplicationsRange,
 }) {
   return (
     <Fragment>
       <h3>Parameters</h3>
-      <label>
-        Attribute:{" "}
-        <FlexibleNumericInput
-          name="attribute"
-          min={attributeMin}
-          max={attributeMax}
-          value={attribute}
-          setValue={setAttribute}
-        />
-      </label>
-      <label>
-        Discipline:{" "}
-        <FlexibleNumericInput
-          name="discipline"
-          min={disciplineMin}
-          max={disciplineMax}
-          value={discipline}
-          setValue={setDiscipline}
-        />
-      </label>
-      <FormControlLabel control={<Switch/>} label="Focus" checked={focus} onChange={(e) => setFocus(e.target.checked)}/>
-      <label>
-        Dice:{" "}
-        <FlexibleNumericInput
-          name="dice"
-          type="number"
-          min={numDiceMin}
-          max={numDiceMax}
-          value={numDice}
-          setValue={setNumDice}
-        />
-      </label>
-      <label>
-        Complications Range:{" "}
-        <FlexibleNumericInput
-          name="complications-range"
-          min={complicationsRangeMin}
-          max={complicationsRangeMax}
-          value={complicationsRange}
-          setValue={setComplicationsRange}
-        />
-      </label>
-      <FormControlLabel control={<Switch checked={normalize} onChange={(e) => setNormalize(e.target.checked)}/>} label="normalize"/>
+      <Slider
+        defaultValue={attributeMin}
+        min={attributeMin}
+        max={attributeMax}
+        value={Number(attribute)}
+        onChange={(event, newValue) => {
+          setAttribute(newValue);
+        }}
+        marks
+        valueLabelDisplay="auto"
+      />
+      <Slider
+        defaultValue={disciplineMin}
+        min={disciplineMin}
+        max={disciplineMax}
+        value={Number(discipline)}
+        onChange={(event, newValue) => {
+          setDiscipline(newValue);
+        }}
+        marks
+        valueLabelDisplay="auto"
+      />
+      <FormControlLabel
+        control={<Switch />}
+        label="Focus"
+        checked={focus}
+        onChange={(e) => setFocus(e.target.checked)}
+      />
+      <Slider
+        defaultValue={Number(numDice)}
+        min={Number(numDiceMin)}
+        max={Number(numDiceMax)}
+        value={Number(numDice)}
+        onChange={(event, newValue) => {
+          setNumDice(newValue);
+        }}
+        marks
+        valueLabelDisplay="auto"
+      />
+      <Slider
+        defaultValue={Number(complicationsRange)}
+        min={Number(complicationsRangeMin)}
+        max={Number(complicationsRangeMax)}
+        value={Number(complicationsRange)}
+        onChange={(event, newValue) => {
+          setComplicationsRange(newValue);
+        }}
+        marks
+        valueLabelDisplay="auto"
+      />
+      <FormControlLabel
+        control={
+          <Switch
+            checked={normalize}
+            onChange={(e) => setNormalize(e.target.checked)}
+          />
+        }
+        label="normalize"
+      />
     </Fragment>
   );
 }
